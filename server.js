98f125a282e4ca72fb83b922765267d7e1600808
louiseDepone <118156909+louiseDepone@users.noreply.github.com>
2024-03-12 03:48:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 
 app.use(bodyParser.json());
-// restarting hays
+// restarting hays , again
 app.get("/hello", (req, res) => {
   res.status(200).json({ message: `Hello there!` });
 });
